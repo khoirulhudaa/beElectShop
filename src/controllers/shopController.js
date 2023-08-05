67f72248958ff951c5fcf7d0dcb0c4ce236e823d
valclassdevelop <developervalclass.com>
2023-08-05 11:34:14 +0700

    create restapi signup seller

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -8,6 +8,7 @@ const createShop = async (req, res) => {
         const { seller_name, email_seller, password, telephone_seller } = req.body 
         
         // Cek email apakah sudah ada ?
+        
         const equalEmail = await shopModel.findOne({email_seller})
 
         if(equalEmail) return res.json({ status: 401, message: 'Email already exist!' })
@@ -67,7 +68,7 @@ const getAllShop = async (req, res) => {
         return res.json({ status: 200, data: dataShop })
 
     } catch (error) {
-        return res.json({ status: 500, message: error.message })
+        return res.json({ status: 500, message: error})
     }
 }
 
