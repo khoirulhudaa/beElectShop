d92a6a547a0f8a81af2bfdbb8608b0acd09d82a4
valclassdevelop <developervalclass.com>
2023-08-28 16:53:00 +0700

    konfigurasi multer (uploads) electshop

--- src/routes/shopRouters.js
+++ src/routes/shopRouters.js
@@ -4,6 +4,7 @@ const shopController = require('../controllers/shopController')
 
 router.post('/', shopController.upload.single('image'), shopController.createShop)
 router.get('/', shopController.getAllShop)
+router.put('/:shop_id', shopController.updateShop)
 router.post('/:shop_id', shopController.removeShopById)
 
 module.exports = router
