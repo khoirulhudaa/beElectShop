5bdce0331d9c7724c84b8e0fa2514970351b72c6
valclassdevelop <developervalclass.com>
2023-08-29 10:59:34 +0700

    backend electshop

--- src/routes/shopRouters.js
+++ src/routes/shopRouters.js
@@ -4,7 +4,7 @@ const shopController = require('../controllers/shopController')
 
 router.post('/', shopController.createShop)
 router.get('/', shopController.getAllShop)
-router.put('/:shop_id', shopController.upload.single('image'), shopController.updateShop)
+router.put('/:shop_id', shopController.upload.single('image_shop'), shopController.updateShop)
 router.post('/:shop_id', shopController.removeShopById)
 
 module.exports = router
