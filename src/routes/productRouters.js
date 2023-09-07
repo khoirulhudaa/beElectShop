dbc9c402c6e31944302ebbd774437d4b579e70b1
valclassdevelop <developervalclass.com>
2023-09-07 14:46:34 +0700

    backend electshop

--- src/routes/productRouters.js
+++ src/routes/productRouters.js
@@ -5,5 +5,6 @@ const router = express.Router()
 router.post('/', productControllers.upload.single('product_image'), productControllers.createProduct)
 router.get('/', productControllers.getAllProducts)
 router.delete('/:product_id', productControllers.removeProductById)
+router.post('/:product_id', productControllers.updateProduct)
 
 module.exports = router
