35998833ab3b3c15c7592a17ce1095a2a99f3993
valclassdevelop <developervalclass.com>
2023-09-11 09:50:55 +0700

    authContrller fix electshop

--- src/routes/authRouters.js
+++ src/routes/authRouters.js
@@ -9,11 +9,11 @@ router.post('/signup/seller', authControllers.signUpSeller)
 router.post('/signin/seller', authControllers.signInSeller)
 
 // Get list users
-router.get('/consumer', authControllers.getAllConsumer)
-router.get('/seller', authControllers.getAllSeller)
+// router.get('/consumer', authControllers.getAllConsumer)
+// router.get('/seller', authControllers.getAllSeller)
 
 // Delete Account
-router.delete('/consumer/:consumer_id', authControllers.removeConsumer)
-router.delete('/seller/:seller_id', authControllers.removeSeller)
+// router.delete('/consumer/:consumer_id', authControllers.removeConsumer)
+// router.delete('/seller/:seller_id', authControllers.removeSeller)
 
 module.exports = router
