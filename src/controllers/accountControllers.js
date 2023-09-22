4e52e8e3b27e931ab075783d7ad3ec7e6f7ed14b
valclassdevelop <developervalclass.com>
2023-09-22 08:41:40 +0700

    create check data

--- src/controllers/accountControllers.js
+++ src/controllers/accountControllers.js
@@ -75,15 +75,8 @@ const signUpSeller = async (req, res) => {
     try {
         const { email_seller, seller_name, gender, telephone_seller, password } = req.body
         
-        // Membuat array yang berisi nama-nama field yang harus ada
-        const requiredFields = ['email_seller', 'seller_name', 'gender', 'telephone_seller', 'password'];
-
-        // Mengecek apakah semua field yang dibutuhkan ada dan tidak kosong
-        const missingFields = requiredFields.filter(field => req.body[field] === "");
-
-        // Jika ada field yang kosong, kirim respons error 401
-        if (missingFields.length > 0) {
-            return res.status(401).json({ error: `Fields are missing: ${missingFields.join(', ')}` });
+        if (!email_seller || !seller_name || !gender || !telephone_seller || !password) {
+            return res.status(401).json({ status: 401, message: 'Fields are missing!', data: req.body });
         }
 
         const equalSeller = await Seller.findOne({ email_seller })
