adda982e5dbdf7179051d23f7bf77d006574ef52
valclassdevelop <developervalclass.com>
2023-09-24 15:49:10 +0700

    add middleware token

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,16 +1,17 @@
 const jsonwebtoken = require('jsonwebtoken')
 
 const checkToken = (req, res, next) => {
-    const token = req.header('Authorization')
+    const token = req.header('Authorization');
 
-    if(!token) return res.json({ status: 401, message: 'Toekn Not Found!' })
+    if(!token) return res.json({ status: 401, message: 'Token Not Found!' })
 
     try {
-        jsonwebtoken.verify(token, 'electShop')
+        const result = jsonwebtoken.verify(token, 'ElectShop')
+        req.user = result
         next()
     } catch (error) {
         return res.json({ status: 500, message: error.message })
     }
 }
 
-module.exports = checkToken 
+module.exports = { checkToken } 
