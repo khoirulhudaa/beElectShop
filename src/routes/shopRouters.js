3af3cf0e8f3bdc73fb2484be0db1a755cc8f1912
valclassdevelop <developervalclass.com>
2023-10-11 09:58:41 +0700

    fix params get shop

--- src/routes/shopRouters.js
+++ src/routes/shopRouters.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const shopController = require('../controllers/shopController')
 
 router.post('/', shopController.upload.single('image_shop'), shopController.createShop)
-router.get('/:shop_id', shopController.getAllShop)
+router.get('/:seller_id', shopController.getAllShop)
 router.post('/:shop_id', shopController.upload.single('image_shop'), shopController.updateShop)
 router.delete('/:shop_id', shopController.removeShopById)
 
