c70a58449c14fc512617403d49082845cebd9a23
valclassdevelop <developervalclass.com>
2023-10-11 07:36:55 +0700

    fix error request timeout

--- src/routes/shopRouters.js
+++ src/routes/shopRouters.js
@@ -4,7 +4,7 @@ const shopController = require('../controllers/shopController')
 
 router.post('/', shopController.upload.single('image_shop'), shopController.createShop)
 router.get('/', shopController.getAllShop)
-router.post('/:shop_id', shopController.upload.single('image_shop'), shopController.updateShop)
+router.get('/:shop_id', shopController.upload.single('image_shop'), shopController.updateShop)
 router.delete('/:shop_id', shopController.removeShopById)
 
 module.exports = router
