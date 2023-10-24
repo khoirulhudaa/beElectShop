697a4cf90686a6001c0c0bf3f95db407d4623a23
valclassdevelop <developervalclass.com>
2023-10-24 14:20:27 +0700

    fix bug

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -5,7 +5,9 @@ const fs = require('fs')
 const path = require('path')
 
 const uploadDir = path.join(__dirname, '../uploads');
-fs.mkdirSync(uploadDir, { recursive: true });
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
