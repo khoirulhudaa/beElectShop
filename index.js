ae80843843e29f5fe16fae94870020c2b2aecbb2
valclassdevelop <developervalclass.com>
2023-10-24 13:46:58 +0700

    fix bug

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'src/uploads')));
 
