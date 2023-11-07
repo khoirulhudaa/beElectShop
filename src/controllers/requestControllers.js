151e2ea178f71d9cbf3843eaa879dddd1ce29b1b
valclassdevelop <developervalclass.com>
2023-11-07 10:31:05 +0700

    request feature

--- src/controllers/requestControllers.js
+++ src/controllers/requestControllers.js
@@ -6,24 +6,24 @@ const createRequest = async (req, res) => {
     try {
         const { email_consumer, requestMessage } = req.body
 
-        const equalEmail = await Consumer.findOne({ email_consumer })
-        if(!equalEmail) return res.json({ status: 404, message: 'User not found!' })
+        const user = await Consumer.findOne({ email_consumer })
+        if(!user) return res.json({ status: 404, message: 'User not found!' })
 
-        if(!email_consumer || !requestMessage) return res.json({ tatus: 401, message: 'Incomplete data provided' })
+        if(!email_consumer || !requestMessage) return res.json({ status: 401, message: 'Incomplete data provided' })
 
         const request_id = crypto.randomBytes(20).toString('hex')
 
-        const createRequest = new Request({
+        const createRequestMessage = new Request({
             request_id,
             email_consumer,
             requestMessage
         })
 
-        await createRequest.save() 
+        await createRequestMessage.save() 
         return res.json({ status: 200, message: 'Successfully send your request!' })
 
     } catch (error) {
-        return res.json({ status: 500, message: 'Servor Error!', error: error.message })
+        return res.json({ status: 500, message: 'Server Error!', error: error.message })
     }
 }
 
