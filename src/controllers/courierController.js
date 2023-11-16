7207cc33c075c36573c1b7d4f695e6fb0ef1ca75
valclassdevelop <developervalclass.com>
2023-11-16 23:01:35 +0700

    rajaongkir integration

--- src/controllers/courierController.js
+++ src/controllers/courierController.js
@@ -1,16 +1,27 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const RajaOngkir = require('rajaongkir-nodejs').Starter(process.env.RAJAONGKIR_API_KEY);
+
 const getProvince = async (req,  res) => {
-    try {
+    try {    
+        const response = RajaOngkir.getProvinces()
         
+        return res.json({ status: 200, message: 'Successfully get data all provincy', data: response })
     } catch (error) {
-        
+        return res.json({ status: 500, message: 'Server error!', error: error.message })
     }
 }
 
 const getCity = async (req, res) => {
     try {
+        const { id } = req.params
+
+        const response = RajaOngkir.getCity(id)
         
+        return res.json({ status: 200, message: 'Successfully get data all city', data: response })
     } catch (error) {
-        
+        return res.json({ status: 500, message: 'Server error!', error: error.message })
     }
 }
 
