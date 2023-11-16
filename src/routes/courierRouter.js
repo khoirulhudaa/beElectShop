0c2127946f97a3e7c6985f761a6be9431d07a473
valclassdevelop <developervalclass.com>
2023-11-16 23:29:09 +0700

    rajaongkir integration

--- src/routes/courierRouter.js
+++ src/routes/courierRouter.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const courierController = require('../controllers/courierController')
 
 router.get('/province', courierController.getProvince)
-router.get('/city', courierController.getCity)
+router.get('/city/:id', courierController.getCity)
 
 module.exports = router
