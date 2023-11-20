6fac9cc83823ba68ee013f86d3903cf50ca846c9
valclassdevelop <developervalclass.com>
2023-11-20 15:13:17 +0700

    payment requiest

--- src/routes/paymentRouters.js
+++ src/routes/paymentRouters.js
@@ -5,7 +5,7 @@ const paymentController = require('../controllers/paymentControllers')
 router.get('/:shop_id', paymentController.getAllPaymentByShop)
 router.put('/:shop_id', paymentController.updatePaymentMethod)
 router.post('/withdraw', paymentController.disbursementPayment)
-router.post('/callback', paymentController.handlePaymentCallback)
+router.get('/callback', paymentController.handlePaymentCallback)
 router.post('/cancel/:external_id', paymentController.cancelOrder)
 router.post('/create', paymentController.createPayment)
 
