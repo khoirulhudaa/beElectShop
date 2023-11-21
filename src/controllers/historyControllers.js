a09a7bca245928519d4a5a0b65c0203adea68b09
valclassdevelop <developervalclass.com>
2023-11-21 13:04:41 +0700

    history

--- src/controllers/historyControllers.js
+++ src/controllers/historyControllers.js
@@ -4,8 +4,6 @@ const historyModelSeller = require('../models/historyInSellerModel')
 const removeHistoryConsumer = async (req, res) => {
     try {
         const { history_id, idCart } = req.params
-        console.log('history_id:', history_id)
-        console.log('idCart:', idCart)
 
         const history = await historyModelConsumer.findOne({ history_id })
         if(history === 0) return res.json({ status: 404, message: 'History not found!' })
@@ -15,10 +13,14 @@ const removeHistoryConsumer = async (req, res) => {
             return res.json({ status: 404, message: 'Product not found in history!' });
         }
 
-        history.products.splice(indexProducts, 1)
-        await history.save();
-
-        return res.json({ status: 200, message: `Successfully remove history by idCart: ${idCart}!` })
+        if (history.products.length === 1) {
+            await historyModelConsumer.deleteOne({ history_id });
+            return res.json({ status: 200, message: 'Successfully removed history document!' });
+        } else {
+            history.products.splice(indexProducts, 1)
+            await history.save();
+            return res.json({ status: 200, message: `Successfully removed product with idCart: ${idCart} from history!` });
+        }
 
     } catch (error) {
         return res.json({ status: 500, message:'Failed to remove history!', error: error.message })
