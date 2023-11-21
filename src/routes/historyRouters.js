e132ac86dc479b194721d3739f9a11f06fc0e1ed
valclassdevelop <developervalclass.com>
2023-11-21 09:36:58 +0700

    history

--- src/routes/historyRouters.js
+++ src/routes/historyRouters.js
@@ -2,7 +2,7 @@ const express = require('express')
 const historyController = require('../controllers/historyControllers')
 const router = express.Router()
 
-router.get('/:id', historyController.getAllHistory)
+router.get('/:id?', historyController.getAllHistory)
 router.delete('/:history_id', historyController.removeHistory)
 
 module.exports = router
