254b5b9dbc462219adb34699e4bc8cbec1ba5c0c
valclassdevelop <developervalclass.com>
2023-11-23 00:03:08 +0700

    disbursement

--- src/controllers/historyControllers.js
+++ src/controllers/historyControllers.js
@@ -74,8 +74,30 @@ const getAllHistory = async (req, res) => {
     }
 }
 
+const getAllHistoryBySeller = async (req, res) => {
+    try {  
+        const { id } = req.params
+        const filter = {
+            $or: [
+                { shop_id: id },
+                { consumer_id: id }
+            ]
+        };
+
+        const data = await historyModelSeller.find(filter)
+        
+        if(data.length === 0) return res.json({ status: 404, message: 'Data history not found!' })
+        
+        return res.json({ status: 200, message: 'Successfully get data history!', data })
+
+    } catch (error) {
+        return res.json({ status: 500, message: 'Failed to get history!', error: error.message })
+    }
+}
+
 module.exports = {
     removeHistoryConsumer,
     removeHistorySeller,
-    getAllHistory
+    getAllHistory,
+    getAllHistoryBySeller
 }
--- src/routes/historyRouters.js
+++ src/routes/historyRouters.js
@@ -3,6 +3,7 @@ const historyController = require('../controllers/historyControllers')
 const router = express.Router()
 
 router.get('/:id?', historyController.getAllHistory)
+router.get('/seller/:id?', historyController.getAllHistoryBySeller)
 router.delete('/consumer/:history_id/product/:idCart', historyController.removeHistoryConsumer)
 router.delete('/seller/:history_id/product/:idCart', historyController.removeHistorySeller)
 
